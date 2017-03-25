395271bb1982c7f7995c0301a3c422c5d04ac5a5
Derek Moore <derek@madebysa.com>
2017-03-24 21:03:04 -0700

    Base is done!

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,24 @@
 $( document ).ready(function() {
 
-   $('#items li a').on('click', function(e) {
-      $(this).remove();
-      e.preventDefault();
-    });
 
+  $('button').click(function() {
+    var new_task = $('#input').val();
+    $('#items').prepend('<li><a href="#">'+new_task+'</a></li>');
+    $('#input').val('');
+    return false;
+  });
+
+  // Complete task
+  $('body').on('click', '#items li a', function() {
+    $(this).remove();
+    e.preventDefault();
+  });
+
+  // Clear input on select
+  $('#input').click(
+    function(){
+    $(this).val('');
+  });
 
 
 });
-
