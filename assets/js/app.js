2fded31f82dedb3af4c593a073c29303955b4a35
Derek Moore <derek@madebysa.com>
2018-02-12 17:39:34 -0800

    Testing mobile content editable fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -49,5 +49,17 @@ function startup () {
   self.setInterval (function () {savetext ()}, 1000); //call every second
 }
 
+$(function(){
+
+ // WebKit contentEditable focus bug workaround:
+ if(/AppleWebKit\/([\d.]+)/.exec(navigator.userAgent)) {
+  var editableFix = $('<input style="width:1px;height:1px;border:none;margin:0;padding:0;" tabIndex="-1">').appendTo('html');
+  $('[contenteditable]').blur(function () {
+      editableFix[0].setSelectionRange(0, 0);
+      editableFix.blur();
+  });
+ }
+});
+
 // Run app
 startup ();
